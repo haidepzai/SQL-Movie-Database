79103627e0e13a7fd33d17f734a5867b62acd989
Dubiel Sebastian <sd114@r133-86.pool.mintern.mi.hdm-stuttgart.de>
2019-06-19 16:31:02 +0200

    added References

--- MovieDB.sql
+++ MovieDB.sql
@@ -1,12 +1,13 @@
-DROP TABLE director;
+DROP TABLE movies;
+DROP TABLE prodCompany;
 DROP TABLE role;
 DROP TABLE quote;
 DROP TABLE actor;
-DROP TABLE address;
 DROP TABLE genre;
-DROP TABLE prodCompany;
-DROP TABLE movie_database;
 DROP TABLE movie_grosses;
+DROP TABLE director;
+DROP TABLE address;
+
 
 CREATE TABLE director
 (
@@ -36,7 +37,7 @@ CREATE TABLE address
     address_id INTEGER,
     postCode   VARCHAR(10) NOT NULL,
     street     VARCHAR(40) NOT NULL,
-    location   VARCHAR(40) NOT NULL,
+    test_location   VARCHAR(40) NOT NULL,
     country    VARCHAR(40) NOT NULL
 );
 CREATE TABLE genre
@@ -56,18 +57,18 @@ CREATE TABLE movie_grosses(
     blueRay FLOAT,
     movie_theater FLOAT
 );
-CREATE TABLE movie_database
+CREATE TABLE movies
 (
     movie_id INTEGER,
     title    VARCHAR(40) NOT NULL,
     m_comp_id INTEGER NOT NULL,
-    m_dir_id INTEGER NOT NULL,
+    m_dir_id VARCHAR(40) NOT NULL,
     m_genre_id INTEGER NOT NULL,
-    m_actor_id INTEGER NOT NULL,
-    plot_outline VARCHAR(500)
-
+    m_actor_id VARCHAR(40) NOT NULL,
+    m_grosses_id INTEGER NOT NULL,
+    plot_outline VARCHAR(500),
+    release_date DATE
 );
-
 ALTER TABLE director ADD CONSTRAINT director_pk PRIMARY KEY (dir_socSecNum);
 ALTER TABLE actor ADD CONSTRAINT actor_pk PRIMARY KEY (actor_socSecNum);
 ALTER TABLE role ADD CONSTRAINT role_pk PRIMARY KEY (role_id);
@@ -77,6 +78,11 @@ ALTER TABLE genre ADD CONSTRAINT genre_pk PRIMARY KEY (genre_id);
 ALTER TABLE prodCompany ADD CONSTRAINT prodCompany_pk PRIMARY KEY (comp_id);
 ALTER TABLE movie_grosses ADD CONSTRAINT  movie_gross_pk PRIMARY KEY (grosses_id);
 
+ALTER TABLE prodCompany ADD CONSTRAINT prodCompany_fk1 FOREIGN KEY (p_address_id) REFERENCES address (address_id);
+ALTER TABLE movies ADD CONSTRAINT movies_fk1 FOREIGN KEY (m_comp_id) REFERENCES  prodCompany (comp_id);
+ALTER TABLE movies ADD CONSTRAINT movies_fk2 FOREIGN KEY (m_dir_id) REFERENCES   director (dir_socSecNum);
+ALTER TABLE movies ADD CONSTRAINT movies_fk3 FOREIGN KEY (m_genre_id) REFERENCES  genre (genre_id);
+ALTER TABLE movies ADD CONSTRAINT movies_fk4 FOREIGN KEY (m_actor_id) REFERENCES  actor(actor_socSecNum);
+ALTER TABLE movies ADD CONSTRAINT movies_fk5 FOREIGN KEY (m_grosses_id) REFERENCES movie_grosses(grosses_id);
+
 COMMIT;
-
-
