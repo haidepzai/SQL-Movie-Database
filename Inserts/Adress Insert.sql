0fba783951b81912239e48372595d59b2f0e645f
Dubiel Sebastian <sd114@r133-86.pool.mintern.mi.hdm-stuttgart.de>
2019-06-19 16:45:28 +0200

    Bit of structure

---
+++ Inserts/Adress Insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO address (address_id, postCode, street, test_location, country) VALUES (1,13629,'Langestraße 24','Berlin','Germany');
+INSERT INTO address (address_id, postCode, street, test_location, country) VALUES (2,91522,'New York Street 56','New York','USA');
+INSERT INTO address (address_id, postCode, street, test_location, country) VALUES (3,90187,'Saas Street 31','Los Angeles','USA');
+INSERT INTO address (address_id, postCode, street, test_location, country) VALUES (4,92364,'Old Town Road 42','Dublin','Ireland');
+
---
+++ Inserts/Genre Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO genre (genre_id, genre_name) VALUES (1,'Action');
+INSERT INTO genre (genre_id, genre_name) VALUES (2,'Drama');
+INSERT INTO genre (genre_id, genre_name) VALUES (3,'Abenteuerfilm');
+INSERT INTO genre (genre_id, genre_name) VALUES (4,'Thriller');
+INSERT INTO genre (genre_id, genre_name) VALUES (5,'Musik');
+INSERT INTO genre (genre_id, genre_name) VALUES (6,'Fantasy');
+INSERT INTO genre (genre_id, genre_name) VALUES (7,'Science-Fiction');
+INSERT INTO genre (genre_id, genre_name) VALUES (8,'Horror');
+INSERT INTO genre (genre_id, genre_name) VALUES (9,'Western');
+
+
