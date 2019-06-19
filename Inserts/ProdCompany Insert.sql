c2c9f0dcdc76361c3bbc28352a962acdd0cf343c
lh108 <1lM$d=Hw!!>
2019-06-19 16:50:10 +0200

    Inserts + ER upgrade

---
+++ Inserts/ProdCompany Insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO prodCompany (comp_id, comp_name, p_address_id) VALUES (1, 'The Dalt Wisney Company', 2);
+INSERT INTO prodCompany (comp_id, comp_name, p_address_id) VALUES (2, 'Warner Sis.', 4);
+INSERT INTO prodCompany (comp_id, comp_name, p_address_id) VALUES (3, 'Parodont Pictures', 3);
+INSERT INTO prodCompany (comp_id, comp_name, p_address_id) VALUES (4, '21st Century Fox', 1);
+
---
+++ Inserts/Quote Insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO quote (quote_id, quote_phrase) VALUES (1, '"Summer is coming..."');
+INSERT INTO quote (quote_id, quote_phrase) VALUES (2, '"I am your mother."');
+INSERT INTO quote (quote_id, quote_phrase) VALUES (3, '"Hello. My name is Debastian Subiel. You killed my father. Prepare to die."');
+INSERT INTO quote (quote_id, quote_phrase) VALUES (4, '"To infinity and beyond!"');
